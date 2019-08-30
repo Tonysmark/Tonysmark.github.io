b3a3496b64322f63ddc361b538f11de9cad98df9
tonysmark <mjyuyu@qq.com>
2019-08-30 15:55:22 +0800

    publish dist file

---
+++ dist/precache-manifest.f23f245c6aad0ef9b2511f1e49021f2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e8b59a6ee4461db5afb",
+    "url": "/js/about.d60b1f01.js"
+  },
+  {
+    "revision": "ee5665c07987b34011ff",
+    "url": "/css/app.928b75e0.css"
+  },
+  {
+    "revision": "ee5665c07987b34011ff",
+    "url": "/js/app.42a66fa1.js"
+  },
+  {
+    "revision": "31ce783f68dc82fb9dc4",
+    "url": "/js/chunk-vendors.db2ac110.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "87a9aec2753058e0e4a7504bc8099ea2",
+    "url": "/index.html"
+  }
+];
